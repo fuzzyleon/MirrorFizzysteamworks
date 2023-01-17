b01a57cae7d8049f4e5342135900f549c60f450d
fuzzyleon <79373763+fuzzyleon@users.noreply.github.com>
2023-01-17 17:37:34 +0100

    Update SteamLobby.cs

--- SteamLobby.cs
+++ SteamLobby.cs
@@ -9,13 +9,12 @@ public class SteamLobby : MonoBehaviour
 {
     public static SteamLobby instance;
 
-    //callbacks
+    //Callbacks
     protected Callback<LobbyCreated_t> LobbyCreated;
     protected Callback<GameLobbyJoinRequested_t> JoinRequest;
     protected Callback<LobbyEnter_t> LobbyEntered;
-    protected Callback<LobbyChatUpdate_t> lobbyChatUpdated;
 
-    //lobbies Callbacks
+    //Lobbies callbacks
     protected Callback<LobbyMatchList_t> LobbyList;
     protected Callback<LobbyDataUpdate_t> LobbyDataUpdated;
 
@@ -26,20 +25,16 @@ public class SteamLobby : MonoBehaviour
     private const string HostAdressKey = "HostAddress";
     private CustomNetworkManager manager;
 
-    public GameObject LeftLobbyChat;
-    public string LeftChatText = "Someone has left the lobby";
-
     private void Start()
     {
         if(!SteamManager.Initialized) { return; }
-        if(instance == null) { instance = this; }
+        if (instance == null) { instance = this; }
 
         manager = GetComponent<CustomNetworkManager>();
 
         LobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
         JoinRequest = Callback<GameLobbyJoinRequested_t>.Create(OnJoinRequest);
         LobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
-        lobbyChatUpdated = Callback<LobbyChatUpdate_t>.Create(onLobbyChatUpdated);
 
         LobbyList = Callback<LobbyMatchList_t>.Create(OnGetLobbyList);
         LobbyDataUpdated = Callback<LobbyDataUpdate_t>.Create(OnGetLobbyData);
@@ -50,9 +45,10 @@ public class SteamLobby : MonoBehaviour
         SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypePublic, 5); //2de = friends only, public of invite, 3 = manager.maxConnections aanpassen naar 5 voor max spelers
     }
 
+    //called on creating lobby
     private void OnLobbyCreated(LobbyCreated_t callback)
     {
-        if(callback.m_eResult != EResult.k_EResultOK) { return; }
+        if (callback.m_eResult != EResult.k_EResultOK) { return; }
 
         Debug.Log("Lobby created Succesfully");
 
@@ -62,67 +58,63 @@ public class SteamLobby : MonoBehaviour
         SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "name", SteamFriends.GetPersonaName().ToString() + "'s lobby");
     }
 
+    //called on requestion to join lobby
     private void OnJoinRequest(GameLobbyJoinRequested_t callback)
     {
-        Debug.Log("Request To Join Lobby");
+        Debug.Log("Request to join lobby");
+
+        //Allow players to join
         SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
     }
-
+    //called on entering lobby
     private void OnLobbyEntered(LobbyEnter_t callback)
     {
-        //Everyone
         CurrentLobbyID = callback.m_ulSteamIDLobby;
 
-        //Clients
-        if(NetworkServer.active) { return; }
-
+        //Just for the clients als network niet active is return
+        if (NetworkServer.active) { return; }
+        //Get CSteamID van clients
         manager.networkAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAdressKey);
-
+        //start the client
         manager.StartClient();
-    }
 
-    private void onLobbyChatUpdated(LobbyChatUpdate_t callback)
-    {
-        Debug.Log("left lobby");
     }
 
     public void JoinLobby(CSteamID lobbyID)
     {
         SteamMatchmaking.JoinLobby(lobbyID);
     }
+
     public void LeaveLobby(CSteamID LobbyID)
     {
         SteamMatchmaking.LeaveLobby(LobbyID);
     }
 
-    /*public void SetLobbyOwner(CSteamID lobbyID)
-    {
-        SteamMatchmaking.SetLobbyOwner();
-    }*/
-
     public void GetLobbiesList()
     {
-        if(lobbyIDs.Count > 0) { lobbyIDs.Clear(); }
+        if (lobbyIDs.Count > 0) { lobbyIDs.Clear(); }
 
-        SteamMatchmaking.AddRequestLobbyListResultCountFilter(60);
+        SteamMatchmaking.AddRequestLobbyListResultCountFilter(20);
         SteamMatchmaking.RequestLobbyList();
-    }
 
-    void OnGetLobbyList(LobbyMatchList_t result)
-    {
-        if(LobbiesListManager.instance.listOfLobbies.Count > 0) { LobbiesListManager.instance.DestroyLobbies(); }
-
-        for(int i=0; i < result.m_nLobbiesMatching; i++)
-        {
-            CSteamID lobbyID = SteamMatchmaking.GetLobbyByIndex(i);
-            lobbyIDs.Add(lobbyID);
-            SteamMatchmaking.RequestLobbyData(lobbyID);
-        }
+        Debug.Log("GetLobbiesList from steamlobby script works");
     }
 
     void OnGetLobbyData(LobbyDataUpdate_t result)
     {
         LobbiesListManager.instance.DisplayLobbies(lobbyIDs, result);
+    }
+
+    void OnGetLobbyList(LobbyMatchList_t result)
+    {
+        if (LobbiesListManager.instance.listOfLobbies.Count > 0) { LobbiesListManager.instance.DestroyLobbies(); }
+
+        for (int i = 0; i < result.m_nLobbiesMatching; i++)
+        {
+            CSteamID lobbyID = SteamMatchmaking.GetLobbyByIndex(i);
+            lobbyIDs.Add(lobbyID);
+            SteamMatchmaking.RequestLobbyData(lobbyID);
+        }
     }
 
 
